fb5fad9da6a0c3ed5238703422ca27ba6a1786ec
David Cevallos <david.cevallos.salas@gmail.com>
2025-07-02 14:07:22 -0500

    Advance 1
    Advance 1

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ RUN apk update
 RUN apk add --no-cache python3 py3-pip
 RUN pip3 install flask==2.2.5 --break-system-packages
 RUN pip3 install requests==2.32.3 --break-system-packages
+RUN pip3 install mysql-connector-python==9.3.0 --break-system-packages
 RUN pip3 install pypdf==5.7.0 --break-system-packages
 RUN chmod 777 /dev
 WORKDIR /home/alpine
--- service.py
+++ service.py
@@ -1,8 +1,41 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/test9", methods=["GET"])
+
+@app.route("/loadnormative", methods=["POST"])
+def loadnormative(normative):
+    try:
+        name = request.headers.get("name")
+        with open(name+".pdf","wb") as file:
+            file.write(normative)
+        
+        mydb = mysql.connector.connect(
+            host="192.168.1.86",
+            user="root",
+            password="12345678",
+            database="assessment"
+        )
+
+        mycursor = mydb.cursor()
+
+        sql = "INSERT INTO normatives (id, name, active) VALUES (%s, %s, %s)"
+        val = (1, name, 1)
+        mycursor.execute(sql, val)
+
+        mydb.commit()
+
+        return {"code":0,"message":"OK"}
+    except mysql.connector.Error as error:
+        message = ("Failed to insert into MySQL table {}".format(error))
+        return {"code":-1,"message":message}
+    finally:
+        if mydb.is_connected():
+            mydb.close()
+            mydb.close()
+
+@app.route("/test", methods=["GET"])
 def test():
     return "Api working!"
 
