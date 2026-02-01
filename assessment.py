147c889a5bfcb1811b97c272e32eecdc6878d81c
David Cevallos <david.cevallos.salas@gmail.com>
2026-02-01 16:02:20 -0500

    Enhanced prompts
    Enhanced prompts

--- assessment.py
+++ assessment.py
@@ -24,17 +24,17 @@ def queryGpt(apiKey, id_normative, name_normative, alias_normative, id_law, name
     output2 = "1,5,7,16"
 
     input_system = "You are an expert in privacy and must determine how well aligned is the privacy legal framework " + name_law + " with the standard " + name_normative + ". " \
-    "Given the text of the privacy legal framework, you must determine whether it suggests and/or compels the compliance of each one of the " + str(n) \
-    " categories of subclauses defined by the standard. The categories are coded with numbers from 0 to " + str(n-1) + ". " \
-    "Just return the codes without a reason or extra text. In case of no categories selected, answer None. " \
-    "The categories of subclauses and their definitions are the following:\n" + textNormative + "\n" \
-    "Here, there are two examples of expected output structures from you when analyzing a fragment of another privacy legal framework:\n"
-    "Example 1:\n" \
-    "Text: " + example1 + "\n" \
-    "Expected output structure:" + output1 + "\n" \
-    "Example 2:\n" \
-    "Text: " + example2 + "\n" \
-    "Expected output structure:" + output2
+    + "Given the text of the privacy legal framework, you must determine whether it suggests and/or compels the compliance of each one of the " + str(n) \
+    + " categories of subclauses defined by the standard. The categories are coded with numbers from 0 to " + str(n-1) + ". " \
+    + "Just return the codes without a reason or extra text. In case of no categories selected, answer None. " \
+    + "The categories of subclauses and their definitions are the following:\n" + textNormative + "\n" \
+    + "Here, there are two examples of expected output structures from you when analyzing a fragment of another privacy legal framework:\n" \
+    + "Example 1:\n" \
+    + "Text: " + example1 + "\n" \
+    + "Expected output structure:" + output1 + "\n" \
+    + "Example 2:\n" \
+    + "Text: " + example2 + "\n" \
+    + "Expected output structure:" + output2
         
     input_user =  "The following is the text of the privacy legal framework that you must analyze:\n" + textLaw
 
